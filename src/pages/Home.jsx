1d22f5516030eca63941d675be082d197b9d6c91
Annie Berjou <annie.carrau@wanadoo.fr>
2026-03-23 17:45:10 +0100

    Création composant de la page Html et ajout des images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import Features from "../components/Features/Features";
+import Footer from "../components/Footer/Footer";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
