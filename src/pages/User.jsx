192f3a95d34ff04bc87a35d8c6599fd79aeae70c
Annie Berjou <annie.carrau@wanadoo.fr>
2026-03-23 19:15:09 +0100

    pageUser

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,19 @@
+import Header from "../components/Header/Header";
+import UserHeader from "../components/UserHeader/UserHeader";
+import Accounts from "../components/Accounts/Accounts";
+import Footer from "../components/Footer/Footer";
+
 function User() {
-  return <h1>User Page</h1>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <UserHeader />
+        <Accounts />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default User;
