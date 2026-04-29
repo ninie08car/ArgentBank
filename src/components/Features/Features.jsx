ff48e6f7c329ab0b7174e4aa515f89a25d06affd
Annie Berjou <annie.carrau@wanadoo.fr>
2026-04-29 09:25:28 +0200

    optimisation des images

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -7,20 +7,20 @@ const Features = () => {
       <h2 className="sr-only">Features</h2>
 
       <FeatureItem
-        img="/src/assets/img/icon-chat.png"
+        img="/src/assets/img/icon-chat.webp"
         title="You are our #1 priority"
         text="Need to talk to a representative? You can get in touch through our
         24/7 chat or through a phone call in less than 5 minutes."
       />
 
       <FeatureItem
-        img="/src/assets/img/icon-money.png"
+        img="/src/assets/img/icon-money.webp"
         title="More savings means higher rates"
         text="The more you save with us, the higher your interest rate will be!"
       />
 
       <FeatureItem
-        img="/src/assets/img/icon-security.png"
+        img="/src/assets/img/icon-security.webp"
         title="Security you can trust"
         text="We use top of the line encryption to make sure your data and money
         is always safe."
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import argentBankLogo from "../../assets/img/argentBankLogo.png";
+import argentBankLogo from "../../assets/img/argentBankLogo.webp";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutReducer } from "../../redux/slices/loginSlice";
